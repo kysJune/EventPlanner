d476bd4449c16537b64c40f6833ab51e16ef5cf6
EvanG13 <emgiampaoli@gmail.com>
2024-01-27 17:17:58 -0800

    chore: more setup

---
+++ server/app.ts
@@ -0,0 +1,35 @@
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.get('/hello/cleveland', (req: Request, res: Response) =>{
+    res.send('Hello Cleveland!')
+})
+
+app.get("/test", (req, res) => {
+    interface test {
+        name: string,
+        age: number
+    }
+
+    const person: test = {
+        name: "test",
+        age: 22
+    };
+
+    console.log(person);
+    res.send("tet again");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+});
+
