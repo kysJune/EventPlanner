08e5c372ad7dd47d6cf7fa7913a2a3305b4eab34
EvanG13 <emgiampaoli@gmail.com>
2024-01-29 20:59:40 -0800

    chore: install eslint

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,15 +1,24 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-    overrides: [
-        {
-            "extends": "standard",
-            "rules": {
-                "semi": [1, "always"]
-            }
-        }
-    ]
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"standard",
+		"prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	root: true,
+	overrides: [
+		{
+			files: ["*.ts"], // Add the file patterns here
+			rules: {
+				semi: [1, "always"],
+				quotes: ["error", "double"],
+				"no-console": ["warn"],
+				indent: ["error", "tab"],
+				"comma-dangle": ["error", "always-multiline"],
+			},
+		},
+	],
 };
