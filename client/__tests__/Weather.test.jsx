2df1bca21a4a7f59f4bdfb9fc80596216d85964a
EvanG13 <emgiampaoli@gmail.com>
2024-02-02 18:04:02 -0800

    fix: fix eslint and jest module conflict

--- client/__tests__/Weather.test.jsx
+++ client/__tests__/Weather.test.jsx
@@ -1,6 +1,5 @@
 const { convertKelvinToFahrenheit, getIcon } = require("../src/components/weather/Logic");
 
-// TODO Find out why jest is being annoying
 describe("Gets the weather", () => {
 	it("converts Kelvin to Fahrenheit", () => {
 		expect(convertKelvinToFahrenheit(280)).toBe(44);
