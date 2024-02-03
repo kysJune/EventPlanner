e4ee9829044938aecdb0db501b5bcefc205abb69
EvanG13 <emgiampaoli@gmail.com>
2024-02-02 22:15:47 -0800

    feat: create event service (#8)

--- server/services/database.event.ts
+++ server/services/database.event.ts
@@ -1,30 +1,75 @@
-class DatabaseEvent {
+import { EventResponse } from "../types/responses/eventResponse";
+import { ObjectId } from "mongoose";
+import { EventRequest } from "../types/requests/eventRequest";
+import EventModel from "../models/event";
 
+class DatabaseEvent {
 	/**
 	 * Create an event
 	 */
-	create() {
-		// TODO
+	async create(event: EventRequest) {
+		const newEvent = new EventModel(event);
+		return await newEvent.save();
 	}
 
 	/**
-	 * Get an event
+	 * Get a single event by userid and name
 	 */
-	read() {
-		// TODO
+	async read(
+		userid: ObjectId,
+		name: string,
+	): Promise<EventResponse | undefined> {
+		const event: EventResponse | null = await EventModel.findOne({
+			userid,
+			name,
+		});
+		if (event) {
+			console.info("Event found");
+			return event;
+		} else {
+			console.info("Event not found");
+			return undefined;
+		}
+	}
+
+	/**
+	 *
+	 */
+	async listEvents(
+		userid: ObjectId,
+		day: number,
+		month: number,
+		year: number,
+	): Promise<EventResponse[] | undefined> {
+		const events: EventResponse[] = await EventModel.find({
+			userid,
+			day,
+			month,
+			year,
+		});
+
+		if (events.length > 0) {
+			console.info("Events found");
+			return events;
+		} else {
+			return undefined;
+		}
 	}
 
 	/**
 	 * Update an event
 	 */
-	update() {
-		// TODO
+	async update(
+		id: ObjectId,
+		eventUpdate: Partial<EventRequest>,
+	): Promise<void> {
+		await EventModel.updateOne({ _id: id }, eventUpdate);
 	}
 
 	/**
 	 * Delete a user
 	 */
-	delete() {
-		// TODO
+	async delete(id: ObjectId) {
+		await EventModel.deleteOne({ _id: id });
 	}
 }
