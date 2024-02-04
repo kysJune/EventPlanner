82dd8727dc6ab6ec567753b721dd4259ccde4def
Anjana Nambiar <anjana.n2308@gmail.com>
2024-02-03 22:26:17 -0800

    implements register functionality/setup mongo session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import Weather from "./components/weather/Weather";
 import { Login } from "./components/login/Login";
 import Home from "./components/home/Home";
-// import { Register } from "./components/register/Register";
+import { Register } from "./components/register/Register";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
@@ -11,7 +11,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/Login" element={<Login />} />
-					{/* <Route path="/register" element={<Register/>} />  */}
+					<Route path="/register" element={<Register/>} /> 
 					<Route path="/Weather" element={<Weather />} />
 					<Route path="/" element={<Home />} />
 				</Routes>
