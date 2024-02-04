72756c2c164bb20bad5c2c3755b8ab366e836a56
CleveyP <clevelandplonsey@gmail.com>
2024-02-03 17:56:50 -0800

    implement day component to render all hours. Stick a day component in home to test it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,26 +2,22 @@ import Weather from "./components/weather/Weather";
 import { Login } from "./components/login/Login";
 import Home from "./components/home/Home";
 // import { Register } from "./components/register/Register";
-import {Route, Routes, BrowserRouter} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
-
 	return (
 		<div className="App">
 			<BrowserRouter>
-			<Routes>
-				<Route path="/Login" element={<Login/>} />
-				{/* <Route path="/register" element={<Register/>} />  */}
-				<Route path="/Weather" element={<Weather/>} />
-				<Route path="/" element={<Home/>} />
-			</Routes>
+				<Routes>
+					<Route path="/Login" element={<Login />} />
+					{/* <Route path="/register" element={<Register/>} />  */}
+					<Route path="/Weather" element={<Weather />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
 			</BrowserRouter>
 		</div>
 	);
 }
-
-
-
 
 export default App;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,28 +1,30 @@
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-const Home  = () =>{
-    const navigate = useNavigate();
-    const handleLogin = () => {
-        console.log("login button clicked");
-        navigate("/login");
-    }
+import Day from "../day/Day";
 
-    const handleRegister = () => {
-        navigate("/register");
-    }
+const Home = () => {
+	const navigate = useNavigate();
 
-    return (
-        <div className="home">
-            <header>
-                <nav>
-                    <button onClick = {handleLogin}>login</button>
-                    <button onClick = {handleRegister}>register</button>
-                </nav>
-            </header>
-            <h1>Welcome to the Weather App</h1>
-            
-        </div>
-    )
-}
+	const handleLogin = () => {
+		navigate("/login");
+	};
+
+	const handleRegister = () => {
+		navigate("/register");
+	};
+
+	return (
+		<div className="home">
+			<header>
+				<nav>
+					<button onClick={handleLogin}>login</button>
+					<button onClick={handleRegister}>register</button>
+				</nav>
+			</header>
+			<h1>Welcome to the Weather App</h1>
+			<Day day={1} month={1} year={2022} />
+		</div>
+	);
+};
 
 export default Home;
