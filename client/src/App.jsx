0a87f05d809fc4c9df29c272f6793811e9d19724
CleveyP <clevelandplonsey@gmail.com>
2024-02-03 16:23:16 -0800

    fix: remove vscode merge tracks from package json and App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,11 @@
-<<<<<<< Updated upstream
-import { useState, useEffect } from "react";
-import { listOfUsers } from "../pretendDb.js";
-import User from "./components/User.jsx";
+import Weather from "./components/weather/Weather";
 import "./App.css";
-import Login from "./components/login/login.jsx";
 import Register from "./components/register/Register.jsx";
-import Weather from "./components/weather/Weather.jsx";
-=======
-import { useState, useEffect } from 'react'
-import {listOfUsers} from "../pretendDb.js";
 import { Login } from './components/login/Login.jsx';
 import './App.css'
->>>>>>> Stashed changes
+
 
 function App() {
-	let [count, setCount] = useState(0);
-	const [users, setUsers] = useState([]);
-	const links = ["link1", "link2", "link3"];
-
-	const handleClick = () => {
-		setCount(count + 1);
-	};
-
-	useEffect(() => {
-		// fetch data from server   getAllUsers
-
-<<<<<<< Updated upstream
-		setUsers(listOfUsers);
-	}, []);
-
 	return (
 		<div className="App">
 			<header>
@@ -40,27 +17,6 @@ function App() {
 			<Register />
 		</div>
 	);
-=======
-  useEffect( () => {
-    // fetch data from server   getAllUsers
-    setUsers(listOfUsers);
-  }, [])
-
-  
-  return (
-    <div className="App">
-     <header>
-        <h1>Event Planner</h1>
-        {
-         links.map((link, index) => <a  key = {index} href="#">{link}</a>)
-        }
-     </header>
-     <Login/>
-     <button onClick={handleClick}>increment counter</button>
-     <p id="counter">{count}</p>
-    </div>
-  )
->>>>>>> Stashed changes
 }
 
 export default App;
