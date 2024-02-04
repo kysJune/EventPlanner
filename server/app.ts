22800c52f20a3f8a2ab53354506006877cfc0945
EvanG13 <emgiampaoli@gmail.com>
2024-02-03 19:42:12 -0800

    feat: update app.ts with express sessions

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,11 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectMongo } from "./config/dbConnection";
 import userRouter from "./routes/userRouter";
+import mongoose from "mongoose";
+
+import session from "express-session";
+const MongoStore = require("connect-mongo")(session); // eslint-disable-line @typescript-eslint/no-var-requires
+
 dotenv.config();
 connectMongo();
 
@@ -9,6 +14,16 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 // middleware
+app.use(
+	session({
+		secret: "your-secret-key",
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+		cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+	})
+);
+
 app.use("/user", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
