26c0e005d46de44f2556c08fcec3f60834cf57d8
CleveyP <clevelandplonsey@gmail.com>
2024-02-03 16:55:03 -0800

    setup react-router-dom and create routes in App. Create Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
 import Weather from "./components/weather/Weather";
 import { Login } from "./components/login/Login";
+import Home from "./components/home/Home";
+// import { Register } from "./components/register/Register";
+import {Route, Routes, BrowserRouter} from "react-router-dom";
 import "./App.css";
 
 function App() {
+
 	return (
 		<div className="App">
-			<header>
-				<h1>Event Planner</h1>
-				<nav className="nav-links"></nav>
-			</header>
-			<Weather />
-			<Login />
+			<BrowserRouter>
+			<Routes>
+				<Route path="/Login" element={<Login/>} />
+				{/* <Route path="/register" element={<Register/>} />  */}
+				<Route path="/Weather" element={<Weather/>} />
+				<Route path="/" element={<Home/>} />
+			</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
+
+
+
 
 export default App;
