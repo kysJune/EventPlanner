22cc70611d652d90f247aaf1e4e0bff2732c9c29
CleveyP <clevelandplonsey@gmail.com>
2024-02-07 03:05:22 -0800

    populateMonth algorithm adds 'today' class to the current day if the month being viewed is the current month

--- client/src/components/month/algorithms.js
+++ client/src/components/month/algorithms.js
@@ -1,8 +1,12 @@
 import { WeekDay, getDay, numDaysInMonth } from "../../../../server/utils/CalculateWeekDay";
 
 const populateMonth = (month, year) => {
+	const todayMonth = getCurrentMonth();
+	const todayYear = getCurrentYear();
+	let todayDay = getCurrentDay();
+
 	const firstWeekDay = getDay(1, month, year);
-	
+
 	const numDaysCurr = numDaysInMonth(month, year);
 	const numDaysPrev = numDaysInMonth(
 		month - 1 < 0 ? 11 : month - 1,
@@ -41,7 +45,7 @@ const populateMonth = (month, year) => {
 		});
 	}
 	weeks.push(daysOfLastMonth);
-    //fill the rest of the month
+	//fill the rest of the month
 	loop1: for (let week = 1; week < numWeeks; week++) {
 		const days = [];
 		for (let i = 0; i < 7; i++) {
@@ -57,33 +61,53 @@ const populateMonth = (month, year) => {
 				weekDay: "",
 				monthStatus: "curr"
 			});
-            if (week * 7 + i + 1 - firstWeekDay === numDaysCurr) {
-                weeks.push(days);
-                break loop1;
-            }
+			if (week * 7 + i + 1 - firstWeekDay === numDaysCurr) {
+				weeks.push(days);
+				break loop1;
+			}
 		}
 
 		weeks.push(days);
 	}
-    //fill the remaining days with the next month
-    if ( weeks[weeks.length - 1].length < 7 ) {
-        let stoppingCondition =  7 - weeks[weeks.length - 1].length;
-        for(let i = 1; i <= stoppingCondition; i++) {
-            weeks[weeks.length - 1].push({
-                events: [
-                    {
-                        name: "Rocket League",
-                        start: 0,
-                        end: 24
-                    }
-                ],
-                day: i,
-                weekDay: "",
-                monthStatus: "next"
-            });
-        }
-    }
+	//fill the remaining days with the next month
+	if (weeks[weeks.length - 1].length < 7) {
+		let stoppingCondition = 7 - weeks[weeks.length - 1].length;
+		for (let i = 1; i <= stoppingCondition; i++) {
+			weeks[weeks.length - 1].push({
+				events: [
+					{
+						name: "Rocket League",
+						start: 0,
+						end: 24
+					}
+				],
+				day: i,
+				weekDay: "",
+				monthStatus: "next"
+			});
+		}
+	}
+	//if the month is the current month, mark the current day
+	if (month === todayMonth && year === todayYear) {
+		todayDay += firstWeekDay; //offset the todayDay index by the number of days from previous month
+		weeks[Math.floor((todayDay - 1) / 7)][(todayDay - 1) % 7].monthStatus = "today";
+	}
 	return weeks;
+};
+
+export const getCurrentMonth = () => {
+	const date = new Date();
+	return date.getMonth();
+};
+
+export const getCurrentYear = () => {
+	const date = new Date();
+	return date.getFullYear();
+};
+
+export const getCurrentDay = () => {
+	const date = new Date();
+	return date.getDate();
 };
 
 export default populateMonth;
