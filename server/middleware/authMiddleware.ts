06a4bece51bdab463f51b816b8cb4dc87cf0c2d5
CleveyP <clevelandplonsey@gmail.com>
2024-02-08 18:49:49 -0800

    create authMiddleware that checks if user is logged in and has a session id

---
+++ server/middleware/authMiddleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	if (req.session.isLoggedIn && req.session.userid) {
+		next();
+	} else {
+		res.status(401).send("Unauthorized user. Must be logged in to access this route.");
+	}
+};
+
+export default authMiddleware;
