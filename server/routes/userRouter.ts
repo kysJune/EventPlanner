55d28c98ad696fab55d2b21838728c599760e4a2
CleveyP <clevelandplonsey@gmail.com>
2024-02-15 19:04:59 -0800

    create logout route and controller. Destroys session

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/userControllers";
+import { login, register, logout } from "../controllers/userControllers";
 import { errorHandlerMiddleware } from "../middleware/errorHandler";
 const userRouter = express.Router();
 
 userRouter.post("/login", login, errorHandlerMiddleware);
 userRouter.post("/register", register, errorHandlerMiddleware);
+userRouter.post("/logout", logout, errorHandlerMiddleware);
 
 export default userRouter;
