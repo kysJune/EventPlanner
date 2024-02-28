f5204f0361f2276bbc10c0d2e05f18f6587a0b86
CleveyP <clevelandplonsey@gmail.com>
2024-02-28 14:40:52 -0800

    refactor the Venues component to draw all states from a states.js file

--- client/src/components/venues/Venues.jsx
+++ client/src/components/venues/Venues.jsx
@@ -5,6 +5,7 @@ import Header from "../header/Header";
 import Modal from "react-modal";
 import customAxios from "../../config/customAxios";
 import isValidEvent, { get24HourTime } from "../day/algorithms.js";
+import {states as statesList} from "./states.js";
 
 const Venues = () => {
 	const [venues, setVenues] = useState([]);
@@ -61,56 +62,11 @@ const Venues = () => {
 						onChange={(e) => setEventCity(e.target.value)}
 					/>
 					<select value={eventState} onChange={(e) => setEventState(e.target.value)}>
-						<option value="AL">Alabama</option>
-						<option value="AK">Alaska</option>
-						<option value="AZ">Arizona</option>
-						<option value="AR">Arkansas</option>
-						<option value="CA">California</option>
-						<option value="CO">Colorado</option>
-						<option value="CT">Connecticut</option>
-						<option value="DE">Delaware</option>
-						<option value="FL">Florida</option>
-						<option value="GA">Georgia</option>
-						<option value="HI">Hawaii</option>
-						<option value="ID">Idaho</option>
-						<option value="IL">Illinois</option>
-						<option value="IN">Indiana</option>
-						<option value="IA">Iowa</option>
-						<option value="KS">Kansas</option>
-						<option value="KY">Kentucky</option>
-						<option value="LA">Louisiana</option>
-						<option value="ME">Maine</option>
-						<option value="MD">Maryland</option>
-						<option value="MA">Massachusetts</option>
-						<option value="MI">Michigan</option>
-						<option value="MN">Minnesota</option>
-						<option value="MS">Mississippi</option>
-						<option value="MO">Missouri</option>
-						<option value="MT">Montana</option>
-						<option value="NE">Nebraska</option>
-						<option value="NV">Nevada</option>
-						<option value="NH">New Hampshire</option>
-						<option value="NJ">New Jersey</option>
-						<option value="NM">New Mexico</option>
-						<option value="NY">New York</option>
-						<option value="NC">North Carolina</option>
-						<option value="ND">North Dakota</option>
-						<option value="OH">Ohio</option>
-						<option value="OK">Oklahoma</option>
-						<option value="OR">Oregon</option>
-						<option value="PA">Pennsylvania</option>
-						<option value="RI">Rhode Island</option>
-						<option value="SC">South Carolina</option>
-						<option value="SD">South Dakota</option>
-						<option value="TN">Tennessee</option>
-						<option value="TX">Texas</option>
-						<option value="UT">Utah</option>
-						<option value="VT">Vermont</option>
-						<option value="VA">Virginia</option>
-						<option value="WA">Washington</option>
-						<option value="WV">West Virginia</option>
-						<option value="WI">Wisconsin</option>
-						<option value="WY">Wyoming</option>
+						{
+							Object.keys(statesList).map((stateKey, index) => {
+								return <option key={index} value={stateKey}>{statesList[stateKey]}</option>;
+							})
+						}
 					</select>
 				</div>
 				<label htmlFor="category-input">What are you looking for?</label>
